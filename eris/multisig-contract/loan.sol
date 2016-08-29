9c08bed5f7b3fd7d950a2609fd7a236ef560abfa
Jim Barritt <jim@planet-ix.com>
2016-08-29 13:39:51 +0100

    Full lifecycle working

--- eris/multisig-contract/loan.sol
+++ eris/multisig-contract/loan.sol
@@ -8,6 +8,15 @@ contract LoanContract {
     bool lender;
     bool counterFraud;
   }
+
+  // Our loan system would listen for this so that it could make the loan payment
+  event ReadyToPay(address loanContract);
+
+  modifier onlyExecutor() {
+    if (msg.sender != executor) throw;
+    _
+  }
+
   
   address executor; // The "controlling" entity for the contract  
   address lender;
@@ -18,18 +27,20 @@ contract LoanContract {
 
   SignatureTracking signatures;
 
-  function LoanContract(address _lender, address _counterFraud) {
+  function LoanContract(string _hashOfContract, address _lender, address _counterFraud) {
+    hashOfContract = _hashOfContract;
     lender = _lender;
     counterFraud = _counterFraud;
     executor = msg.sender;
   }
 
-  // Our loan system would listen for this so that it could make the loan payment
-  event ReadyToPay(address loanContract);
-
-  modifier onlyExecutor() {
-    if (msg.sender != executor) throw;
-    _
+  // Can't initialise constructor from epm at the moment - waiting for rc3
+  function initialise (string _hashOfContract, address _lender, address _counterFraud)
+    onlyExecutor()
+  {
+    hashOfContract = _hashOfContract;
+    lender = _lender;
+    counterFraud = _counterFraud;
   }
 
   modifier onlyBorrower() {
@@ -59,14 +70,40 @@ contract LoanContract {
     _
   }
 
-  function initialise (string _hashOfContract, address _lender, address _counterFraud)
-    onlyExecutor()
-  {
-    hashOfContract = _hashOfContract;
-    lender = _lender;
-    counterFraud = _counterFraud;
+  // --------------------
+  // this is copied from https://github.com/ethereum/dapp-bin/blob/master/library/stringUtils.sol
+  // --------------------
+
+  /// @dev Does a byte-by-byte lexicographical comparison of two strings.
+  /// @return a negative number if `_a` is smaller, zero if they are equal
+  /// and a positive numbe if `_b` is smaller.
+  function compare(string _a, string _b) returns (int) {
+    bytes memory a = bytes(_a);
+    bytes memory b = bytes(_b);
+    uint minLength = a.length;
+    if (b.length < minLength) minLength = b.length;
+    //@todo unroll the loop into increments of 32 and do full 32 byte comparisons
+    for (uint i = 0; i < minLength; i ++)
+      if (a[i] < b[i])
+        return -1;
+      else if (a[i] > b[i])
+        return 1;
+    if (a.length < b.length)
+      return -1;
+    else if (a.length > b.length)
+      return 1;
+    else
+      return 0;
   }
 
+  // --------------------
+
+  modifier hashOfContractsMustMatch(string testHash) {
+    if (compare(hashOfContract, testHash) != 0) throw;
+    _
+  }
+
+  
   function addBorrower(address addr)
     onlyExecutor()
   {
@@ -106,17 +143,20 @@ contract LoanContract {
     return signatures.counterFraud;
   }
 
+  
   // The hash gets passed in by each party so we can check it matches the one that we created.
   function sign(string _hashOfContract)
     onlyBorrower()
     inState(State.Created)
-  {
+    hashOfContractsMustMatch(_hashOfContract)
+  {    
     signatures.borrowerCount ++;
   }
 
   function approved(string _hashOfContract)
     onlyCounterFraud()
     inState(State.BorrowerSigned)
+    hashOfContractsMustMatch(_hashOfContract)
   {
     signatures.counterFraud = true;
     ReadyToPay(this);
@@ -126,6 +166,7 @@ contract LoanContract {
    function paid(string _hashOfContract)
     onlyLender()
     inState(State.ReadyToPay)
+    hashOfContractsMustMatch(_hashOfContract)
   {
     signatures.lender = true;
   }
