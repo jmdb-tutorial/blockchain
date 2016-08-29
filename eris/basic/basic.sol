f6b6ae94472e0e4135339a54a31483b1c2a9e5b9
Jim Barritt <jim@planet-ix.com>
2016-08-29 08:38:55 +0100

    Got it to work with an initialise method

--- eris/basic/basic.sol
+++ eris/basic/basic.sol
@@ -1,28 +1,42 @@
 /// @title A multisignatory loan contract
 // TODO: implement a cancel method that could also have some rules
 contract BasicContract {
-  uint stateInteger;
+  uint stateIntA;
+  uint stateIntB;
   string stateString;
 
-  function BasicContract(uint x) {
-    stateInteger = x;
-    stateString = "foo";
+  function BasicContract() {
   }
 
-  function setStateInt(uint x) {
-    stateInteger = x;
+  function initialise(uint _stateIntA, uint _stateIntB, string _stateString) {
+    stateIntA = _stateIntA;
+    stateIntB = _stateIntB;
+    stateString = _stateString;
   }
   
-  function getStateInt() returns (uint state) {
-    return stateInteger;
+  function setStateIntA(uint x) {
+    stateIntA = x;
   }
 
+  function setStateIntB(uint x) {
+    stateIntB = x;
+  }
+  
   function setStateString(string s) {
     stateString = s;
   }
 
-  function getStateString() returns (string s) {
+  function getStateIntA() constant returns (uint) {
+    return stateIntA;
+  }
+  
+  function getStateIntB() constant returns (uint) {
+    return stateIntB;
+  }
+  
+  function getStateString() constant returns (string) {
     return stateString;
   }
+
   
 }
